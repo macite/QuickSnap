0cc5fb7dc0dacfc18f904449de75367d8209b243
Andrew Cain <acain@swin.edu.au>
2015-03-26 13:38:23 +1100

    add Deck to program so that player draws cards from the Deck

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -6,13 +6,14 @@ namespace CardGames
 {
     public class SnapGame
     {
-        private static Card _testCard = Card.RandomCard();
-
         public static void Main()
         {
             //Open the game window
             SwinGame.OpenGraphicsWindow("Snap!", 800, 600);
             
+			Deck myDeck = new Deck ();
+			Card testCard = Card.RandomCard();
+
             //Run the game loop
             while(false == SwinGame.WindowCloseRequested())
             {
@@ -21,12 +22,12 @@ namespace CardGames
                 SwinGame.ClearScreen(Color.White);
                 
                 //generate a random card on spacebar press
-                if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
+				if (SwinGame.KeyTyped(KeyCode.vk_SPACE) && myDeck.CardsRemaining > 0)
                 {
-                    _testCard = Card.RandomCard ();
+					testCard = myDeck.Draw ();
                 }
 
-                SwinGame.DrawText ("Card generated was: " + _testCard.ToString (), Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("Card generated was: " + testCard.ToString (), Color.RoyalBlue, 0, 20);
 
                 //Clear the screen and draw the framerate
                 SwinGame.DrawFramerate(0,0);

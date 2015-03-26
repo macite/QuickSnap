48aa4465b3f0a0e95499f31972892d7af4f2434f
Andrew Cain <acain@swin.edu.au>
2015-03-26 11:27:41 +1100

    add card class and include in Main

---
+++ src/GameLogic/Card.cs
@@ -0,0 +1,130 @@
+using System;
+
+namespace CardGames.GameLogic
+{
+	/// <summary>
+	/// The Suit enumeration provides a list of all of the valid suit values
+	/// for the cards in the program.
+	/// </summary>
+    public enum Suit
+    {
+	    CLUB,
+	    DIAMOND,
+	    HEART,
+	    SPADE
+    }
+    
+	/// <summary>
+	/// The Rank enumeration provides a list of all of the valid ranks (values)
+	/// for the cards in the program. The ACE has value 1, ensuring that it is
+	/// easy to map cards to related numeric values.
+	/// </summary>
+    public enum Rank
+    {
+	    ACE = 1,
+	    TWO,
+	    THREE,
+	    FOUR,
+	    FIVE,
+	    SIX,
+	    SEVEN,
+	    EIGHT,
+	    NINE,
+	    TEN,
+	    JACK,
+	    QUEEN,
+	    KING
+    }
+    
+	/// <summary>
+	/// Each Card has a Rank and a Suit
+	/// </summary>
+	public class Card
+	{
+        private Rank _rank;
+        private Suit _suit;
+        
+		/// <summary>
+		/// Create a new card with the indicated rank and suit.
+		/// </summary>
+		/// <param name="r">The rank value for the card.</param>
+		/// <param name="s">The suit value for the card.</param>
+		public Card (Rank r, Suit s)
+		{
+            _rank = r;
+            _suit = s;
+		}
+        
+		/// <summary>
+		/// Allows you to read the value of the Card's rank.
+		/// </summary>
+		/// <value>The rank.</value>
+        public Rank Rank
+        {
+            get { return _rank; }
+        }
+        
+		/// <summary>
+		/// Allows you to read the value of the Card's suit.
+		/// </summary>
+		/// <value>The suit.</value>
+        public Suit Suit
+        {
+            get { return _suit; }
+        }
+
+		/// <summary>
+		/// Returns a <see cref="System.String"/> that represents the current <see cref="CardGames.Card"/>. This will have
+		/// the first character representing the rank (using T for 10), and then a symbol for the suit. For example, QS is
+		/// the Queen of Spades, 8H is Eight Hearts, TD is Ten Diamonds, and 2C is 2 of Clubs. 
+		/// </summary>
+		/// <returns>A <see cref="System.String"/> that represents the current <see cref="CardGames.Card"/>.</returns>
+        public override string ToString()
+        {
+	        String result = "";
+	
+	        switch (_rank){
+	            case Rank.JACK:
+	                result += "J";
+	                break;
+	            case Rank.QUEEN:
+	                result += "Q";
+	                break;
+	            case Rank.KING:
+	                result += "K";
+	                break;
+	            case Rank.ACE:
+	                result += "A";
+	                break;
+	            case Rank.TEN:
+	                result += "T";
+	                break;
+	            default:
+	                result += (int)_rank;
+                	break;
+	        }
+	
+	        switch (_suit) {
+	            case Suit.CLUB:
+	                result += "C";
+	                break;
+	            case Suit.SPADE:
+	                result += "S";
+	                break;
+	            case Suit.HEART:
+	                result += "H";
+	                break;
+	            case Suit.DIAMOND:
+	                result += "D";
+	                break;
+	            default:
+	                result += "?";
+                	break;
+	        }
+	
+	        return result;
+        }
+        
+	}
+}
+
--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -1,5 +1,6 @@
 using System;
 using SwinGameSDK;
+using CardGames.GameLogic;
 
 namespace CardGames
 {

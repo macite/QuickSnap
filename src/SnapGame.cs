62f8e794b97f27b194b6a83262ee63fdf60d5496
Andrew Cain <acain@swin.edu.au>
2015-03-27 17:21:18 +1100

    add drawing of the scores

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -41,6 +41,8 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else

77155d0ab34fb50475c2a0d43230d87a832a2395
Elena <edleeenna@hotmail.com>
2016-08-11 20:35:07 +1000

    fixing problems with background and text

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -48,8 +48,8 @@ namespace CardGames
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, "GameFont", 0, 20);
 
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 40);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 60);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 
 			}

e061b713e5e65e886110c3a72bdf4a1863f1647a
Elena <edleeenna@hotmail.com>
2016-08-11 20:29:09 +1000

    fixing problems with background and text

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -39,7 +39,7 @@ namespace CardGames
 			SwinGame.DrawBitmap("background.png",0,0);
 
 
-			SwinGame.ClearScreen(Color.White);
+			//SwinGame.ClearScreen(Color.White);
 			//SwinGame.DrawText ("" + myGame.Score (0), Color.White, "GameFont", 0, 30);
 
 			// Draw the top card
@@ -50,12 +50,12 @@ namespace CardGames
 
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.White, "GameFont", 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck

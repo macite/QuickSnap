55cc58a657e218d6390c063566471402ecfcfa89
RandyMarsh1 <Ryanday27@gmail.com>
2016-08-12 21:26:10 +1000

    updated fonts again

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,7 +11,7 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
-			SwinGame.LoadFontNamed("NameFont","ChunkFive.otf",24);        
+			SwinGame.LoadFontNamed("GameFont","Chunkfive.otf",24);        
 		}
 
 		/// <summary>
@@ -55,9 +55,9 @@ namespace CardGames
 			if (top != null)
 			{
 				//SwinGame.DrawText(""+myGame.Score(0), Color.White, "GameFont",0 , 30);
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, "GameFont", 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.Black, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.Black, "GameFont", 0, 60);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else

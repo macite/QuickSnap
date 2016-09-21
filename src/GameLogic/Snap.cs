8a8d5e6ebd26848fd6acf2baaa9ef1cb09fdd093
malinanu <mjjayaweera@gmail.com>
2016-09-22 06:19:26 +0800

    add player hit with left and right shift, with score decrment for miss hits

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -140,6 +140,10 @@ namespace CardGames.GameLogic
 				_score[player]++;
 				//TODO: consider playing a sound here...
 			}
+			else if(player >= 0 && player < _score.Length)
+			{
+				_score[player]--;
+			}
 
 			// stop the game...
 			_started = false;

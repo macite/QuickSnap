7adaeabf000d18d68dbb02aae737e4aa3679e7e6
Teddy Gunawan <teddygunawan55@gmail.com>
2017-03-22 21:06:40 +0900

    Changed the font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,7 +11,8 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
-        }
+			SwinGame.LoadFontNamed ("GameFont","Chunkfive.otf", 24);
+		}
 
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
@@ -54,14 +55,14 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White,"GameFont", 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont",0, 40);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont",0, 60);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.White, "GameFont",0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck

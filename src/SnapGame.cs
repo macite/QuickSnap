ce364d305293181691d8c03ed04b5adcc253724b
harry201vn <harrynguyen201vn@gmail.com>
2019-03-14 18:30:37 +1100

    Add new text font in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,6 +11,7 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.LoadFontNamed("GameFont", "Chunkfive.otf", 12);
         }
 
 		/// <summary>
@@ -40,10 +41,10 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+                SwinGame.DrawText("Top Card is " + top.ToString(),Color.RoyalBlue, "GameFont", 0, 20);
+                SwinGame.DrawText("Player 1 score: " + myGame.Score(0),Color.RoyalBlue, "GameFont", 0, 30);
+                SwinGame.DrawText("Player 2 score: " + myGame.Score(1),Color.RoyalBlue, "GameFont", 0, 40);
+                SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
 			{

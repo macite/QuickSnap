1ce04d4a9fd3034f932a3f91998bd730b9d88f87
1V4NM4X <62086842+1V4NM4X@users.noreply.github.com>
2020-03-12 12:44:56 +0800

    Update SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -34,7 +34,7 @@ namespace CardGames
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
 		private static void DrawGame(Snap myGame)
 		{
-			SwinGame.ClearScreen(Color.White);
+			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
 
 			// Draw the top card
 			Card top = myGame.TopCard;

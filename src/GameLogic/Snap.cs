98d0e936f2f8b5ebe8a9f2aef2ece7fe0045e125
Raymond <raymondlai001221@gmail.com>
2020-03-15 17:31:53 +0800

    add player hit with left and right shift, with scoredecrement for miss hits

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -133,12 +133,14 @@ namespace CardGames.GameLogic
 		public void PlayerHit (int player)
 		{
 			//TODO: consider deducting score for miss hits???
-			if ( player >= 0 && player < _score.Length &&  	// its a valid player
-				 IsStarted && 								// and the game is started
+			if (player >= 0 && player < _score.Length &&    // its a valid player
+				 IsStarted &&                               // and the game is started
 				 _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
 			{
-				_score[player]++;
+				_score [player]++;
 				//TODO: consider playing a sound here...
+			} else if (player >= 0 && player < _score.Length) {
+				_score [player]--;
 			}
 
 			// stop the game...
--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -24,7 +24,16 @@ namespace CardGames
 
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.Start ();
+			}
+			if (myGame.IsStarted) {
+				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					myGame.PlayerHit (0);
+				} else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
+					myGame.PlayerHit (0);
+				} else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
+					myGame.PlayerHit (1);
+				}
 			}
 		}
 

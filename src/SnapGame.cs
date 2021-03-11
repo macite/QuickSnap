4095d0a1a3dc04d60bb3083c1fda00557279df0f
LAI YEE FUNG <101225312@students.swinburne.edu.my>
2021-03-11 14:50:33 +0800

    add new background image and draw it in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -34,8 +34,8 @@ namespace CardGames
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
 		private static void DrawGame(Snap myGame)
 		{
-			SwinGame.ClearScreen(Color.White);
-
+			//SwinGame.ClearScreen(Color.White);
+			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)

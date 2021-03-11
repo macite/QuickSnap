85414a2c4a7b11c283e0407e9ce4b958c660d616
NOOR <noor_ua@yahoo.com>
2021-03-11 07:37:38 +0300

    fix position of the cards to be over background

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -43,11 +43,12 @@ namespace CardGames
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
+
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 155, 153);
 			}
 
 			// Draw the back of the cards... to represent the deck
